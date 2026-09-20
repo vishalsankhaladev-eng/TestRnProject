d2ef8c3402d7c0e751bc35b497fa246f7fed6945
Vihal Sankhala <vishalsankhaladev@gmail..com>
2026-09-20 20:52:48 +0530

    add some text

--- App.tsx
+++ App.tsx
@@ -28,11 +28,7 @@ function AppContent() {
 
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
     <Text>Hello from Main Branch</Text>
-=======
-    <Text>Hello from Feature Branch</Text>
->>>>>>> feature/test-conflict
       <NewAppScreen
         templateFileName="App.tsx"
         safeAreaInsets={safeAreaInsets}
